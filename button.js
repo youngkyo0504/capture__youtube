c86b6322e6544b670b4daab414d1b50ed3cbfc41
kky <keumky2@gamil.com>
2021-04-08 00:15:05 +0900

    update

--- button.js
+++ button.js
@@ -12,6 +12,7 @@ button.addEventListener(
       },
       (injectionResults) => {
         const inserted = document.querySelector(".inserted");
+        //selectvideo에서 반환된 값을 extension의 HTML에 추가해준다.
         inserted.innerHTML = injectionResults[0].result;
       }
     );
@@ -21,22 +22,26 @@ button.addEventListener(
 
 function selectVideo() {
   const b = () => {
+    // 비디오 원본 크기만큼 캔버스태그를 생성해준다.
     var canvas = document.createElement("canvas");
     var video = document.querySelector("video");
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
+
+    // getContext를 이용해서 비디오를 캔버스에 그려준다
     var ctx = canvas.getContext("2d");
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    // 캔버스를 이미지 URL로 만들어서 이미지태그 소스에 넣어준다.
     var dataURI = canvas.toDataURL("image/png");
-    console.log(canvas.height, canvas.width);
     var newImage = document.createElement("img");
     newImage.src = dataURI;
     newImage.classList.add("img");
     newImage.style.width = `${canvas.width * 0.27}px`;
     newImage.style.height = `${canvas.height * 0.27}px`;
-    console.log(newImage.style.width, newImage.style.height);
     newImage.style.display = "block";
     newImage.style.position = "relative";
+    //이미지 태그를 반환
     return newImage.outerHTML;
   };
   return b();
